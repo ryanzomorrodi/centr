02a318a70b53a8f6bcf78d58a889fb9fc79d950f
ryanzomorrodi <ryanzomorrodi@gmail.com>
2024-07-06 12:45:45 -0500

    lintr style changes

--- R/meanCenter.R
+++ R/meanCenter.R
@@ -1,6 +1,6 @@
-lonLat2cartesian <- function(lon, lat) {
-  phi <- ((90 - lat) * pi)/180
-  theta <- (lon * pi)/180
+lonlat_cartesian <- function(lon, lat) {
+  phi <- ((90 - lat) * pi) / 180
+  theta <- (lon * pi) / 180
 
   x <- sin(phi) * cos(theta)
   y <- sin(phi) * sin(theta)
@@ -9,37 +9,37 @@ lonLat2cartesian <- function(lon, lat) {
   list(x = x, y = y, z = z)
 }
 
-cartesian2lonLat <- function(x, y, z) {
+cartesian_lonlat <- function(x, y, z) {
   phi <- acos(z)
-  theta <- asin(y/(sin(phi)))
+  theta <- asin(y / sin(phi))
 
-  lat <- 90 - ((180 * phi)/pi)
-  lon <- (180 * theta)/pi
+  lat <- 90 - ((180 * phi) / pi)
+  lon <- (180 * theta) / pi
 
   list(lon = lon, lat = lat)
 }
 
-cartesianMeanCenter <- function(x, y, z, wts) {
+cartesian_mean <- function(x, y, z, wts) {
   total <- sum(wts)
 
-  x_mean <- sum(x * wts)/total
-  y_mean <- sum(y * wts)/total
-  z_mean <- sum(z * wts)/total
+  x_mean <- sum(x * wts) / total
+  y_mean <- sum(y * wts) / total
+  z_mean <- sum(z * wts) / total
 
-  L <- sqrt(sum((c(x_mean, y_mean, z_mean))^2))
+  l <- sqrt(sum((c(x_mean, y_mean, z_mean))^2))
 
-  surface_x_mean <- x_mean/L
-  surface_y_mean <- y_mean/L
-  surface_z_mean <- z_mean/L
+  surface_x_mean <- x_mean / l
+  surface_y_mean <- y_mean / l
+  surface_z_mean <- z_mean / l
 
   list(x = surface_x_mean, y = surface_y_mean, z = surface_z_mean)
 }
 
-planarMeanCenter <- function (x, y, wts) {
+planar_mean <- function(x, y, wts) {
   total <- sum(wts)
 
-  x_mean <- sum(x * wts)/total
-  y_mean <- sum(y * wts)/total
+  x_mean <- sum(x * wts) / total
+  y_mean <- sum(y * wts) / total
 
   list(x = x_mean, y = y_mean)
 }
@@ -47,24 +47,19 @@ planarMeanCenter <- function (x, y, wts) {
 #' Mean Center
 #'
 #' @description
-#'  `meanCenter()` returns the mean center for each group within
+#'  `mean_center()` returns the mean center for each group within
 #'  a point or polygon simple features object.
-#' 
 #' @section Unprojected data:
 #'  If `st_is_longlat(x)`, mean center is calculated
-#'  assuming a spherical Earth. 
-#' 
+#'  assuming a spherical Earth.
 #' @section Projected data:
 #'  If `!st_is_longlat(x)`, mean center is calculated assuming a
 #' "flat" Earth.
-#' 
 #' @param x Input POINT or POLYGON simple features
 #' @param group specifies groups to calculate individual mean centers
 #'  for
-#' @param weight numeric; weight specifying an individual point's 
+#' @param weight numeric; weight specifying an individual point's
 #'  contribution to the mean center
-#'  
-#' 
 #' @returns An sf object with a mean center for each group
 #' @examples
 #' df <- data.frame(
@@ -73,23 +68,26 @@ planarMeanCenter <- function (x, y, wts) {
 #'   grp = c("a", "b", "a", "b", "a"),
 #'   wt = c(1,5,1,3,2))
 #' x <- sf::st_as_sf(df, coords = c("lon", "lat"), crs = 4326)
-#' meanCenter(x, group = "grp", weight = "wt")
+#' mean_center(x, group = "grp", weight = "wt")
 #' @export
-meanCenter <- function (x, group = NULL, weight = NULL) {
+mean_center <- function(x, group = NULL, weight = NULL) {
+  x_name <- deparse(substitute(x))
+  allowed_geom <- c("POINT", "POLYGON", "MULTIPOINT", "MULTIPOLYGON")
+
   if (!inherits(x, "sf")) {
-    stop(deparse(substitute(x)), " must be an simple features object")
+    stop(x_name, " must be an simple features object")
   }
-  if(any(!(as.character(sf::st_geometry_type(x)) %in% c("POINT", "POLYGON", "MULTIPOINT", "MULTIPOLYGON")))) {
-    stop(deparse(substitute(x)), " must contain only point or polygon geometries")
+  if (any(!(as.character(sf::st_geometry_type(x)) %in% allowed_geom))) {
+    stop(x_name, " must contain only point or polygon geometries")
   }
-  if(is.na(sf::st_crs(x))) {
-    stop(deparse(substitute(x)), " must have a defined projection")
+  if (is.na(sf::st_crs(x))) {
+    stop(x_name, " must have a defined projection")
   }
   if (is.null(weight)) {
     wts <- rep(1, nrow(x))
   } else {
     if (!(weight %in% colnames(x))) {
-      stop(weight, "` doesn't exist within ", deparse(substitute(x)))
+      stop(weight, "` doesn't exist within ", x_name)
     }
     wts <- x[[weight]]
     if (any(is.na(wts))) {
@@ -102,7 +100,7 @@ meanCenter <- function (x, group = NULL, weight = NULL) {
     grps <- rep("a", nrow(x))
   } else {
     if (!(group %in% colnames(x))) {
-      stop("Column `",  group, "` doesn't exist within ", deparse(substitute(x)))
+      stop("Column `",  group, "` doesn't exist within ", x_name)
     }
     grps <- x[[group]]
     if (any(is.na(grps))) {
@@ -119,29 +117,28 @@ meanCenter <- function (x, group = NULL, weight = NULL) {
   names(geometry) <- unique_groups
 
   if (sf::st_is_longlat(x)) {
-    lon <- sf::st_coordinates(x)[,1]
-    lat <- sf::st_coordinates(x)[,2]
-    cartesian <- lonLat2cartesian(lon, lat)
-  
-    centerArgs <- as.data.frame(cartesian)
-    centerArgs$wt <- wts
-    centerArgs_grped <- split(centerArgs, f = grps)
-  
-    for (grp in unique_groups) {
-      meansCart <- do.call(cartesianMeanCenter, centerArgs_grped[[grp]])
-      meansLonLat <- do.call(cartesian2lonLat, meansCart)
-      geometry[[grp]] <- sf::st_point(unlist(meansLonLat))
-    }
-  } else {
-    centerArgs <- data.frame(
-      x = sf::st_coordinates(x)[,1], 
-      y = sf::st_coordinates(x)[,2],
-      wts = wts)
-    centerArgs_grped <- split(centerArgs, f = grps)
+    lon <- sf::st_coordinates(x)[, 1]
+    lat <- sf::st_coordinates(x)[, 2]
+    cartesian <- lonlat_cartesian(lon, lat)
+    ctr_args <- as.data.frame(cartesian)
+    ctr_args$wt <- wts
+    ctr_args_split <- split(centerArgs, f = grps)
 
     for (grp in unique_groups) {
-      meansLonLat <- do.call(planarMeanCenter, centerArgs_grped[[grp]])
-      geometry[[grp]] <- sf::st_point(unlist(meansLonLat))
+      means_cartesian <- do.call(cartesian_mean, ctr_args_split[[grp]])
+      means_lonlat <- do.call(cartesian_lonlat, means_cartesian)
+      geometry[[grp]] <- sf::st_point(unlist(means_lonlat))
+    }
+  } else {
+    ctr_args <- data.frame(
+      x = sf::st_coordinates(x)[, 1],
+      y = sf::st_coordinates(x)[, 2],
+      wts = wts)
+      ctr_args_split <- split(ctr_args, f = grps)
+
+    for (grp in unique_groups) {
+      mean_xy <- do.call(planar_mean, ctr_args_split[[grp]])
+      geometry[[grp]] <- sf::st_point(unlist(mean_xy))
     }
   }
 
@@ -149,8 +146,8 @@ meanCenter <- function (x, group = NULL, weight = NULL) {
     geometry = sf::st_sfc(geometry, crs = sf::st_crs(x))
   ))
   if (!is.null(group)) {
-    output[[group]] = unique_groups
-    output <- output[, c(2,1)]
+    output[[group]] <- unique_groups
+    output <- output[, c(2, 1)]
   }
   output
 }
--- R/medianCenter.R
+++ R/medianCenter.R
@@ -1,50 +1,51 @@
 #' Median Center
-#' 
-#' `medianCenter()` returns the Euclidean median within
-#' a specified tolerance for each group using the method 
+#' `median_center()` returns the Euclidean median within
+#' a specified tolerance for each group using the method
 #' developed by Kuhn and Kuenne 1962.
-#' 
 
-euclideanXYDistance <- function(x, y, x_t, y_t) {
+euclid_xy_dist <- function(x, y, x_t, y_t) {
   sqrt((x - x_t)^2 + (y - y_t)^2)
 }
 
-planarMedianCenterEstimate <- function(x, y, x_t, y_t, wts) {
-  d_t <- euclideanXYDistance(x, y, x_t, y_t)
-  K_t <- wts/d_t
+planar_median_est <- function(x, y, x_t, y_t, wts) {
+  d_t <- euclid_xy_dist(x, y, x_t, y_t)
+  k_t <- wts / d_t
 
-  x_estimate <- sum(K_t * x)/sum(K_t)
-  y_estimate <- sum(K_t * y)/sum(K_t)
+  x_estimate <- sum(k_t * x) / sum(k_t)
+  y_estimate <- sum(k_t * y) / sum(k_t)
 
   list(x = x_estimate, y = y_estimate)
 }
 
-planarMedianCenter <- function(x, y, wts, tol = 0.01) {
-  estimate <- planarMeanCenter(x, y, wts)
-  new_estimate <- planarMedianCenterEstimate(x, y, estimate$x, estimate$y, wts)
+planar_median <- function(x, y, wts, tol = 0.01) {
+  estimate <- planar_mean(x, y, wts)
+  new_estimate <- planar_median_est(x, y, estimate$x, estimate$y, wts)
 
-  while(abs(estimate$x - new_estimate$x) > tol | abs(estimate$y - new_estimate$y) > tol) {
+  while (abs(estimate$x - new_estimate$x) > tol || abs(estimate$y - new_estimate$y) > tol) {
     estimate <- new_estimate
-    new_estimate <- planarMedianCenterEstimate(x, y, estimate$x, estimate$y, wts)
+    new_estimate <- planar_median_est(x, y, estimate$x, estimate$y, wts)
   }
   new_estimate
 }
 
-medianCenter <- function (x, group = NULL, weight = NULL) {
+median_center <- function(x, group = NULL, weight = NULL) {
+  x_name <- deparse(substitute(x))
+  allowed_geom <- c("POINT", "POLYGON", "MULTIPOINT", "MULTIPOLYGON")
+
   if (!inherits(x, "sf")) {
-    stop(deparse(substitute(x)), " must be an simple features object")
+    stop(x_name, " must be an simple features object")
   }
-  if(any(!(as.character(sf::st_geometry_type(x)) %in% c("POINT", "POLYGON", "MULTIPOINT", "MULTIPOLYGON")))) {
-    stop(deparse(substitute(x)), " must contain only point or polygon geometries")
+  if (any(!(as.character(sf::st_geometry_type(x)) %in% allowed_geom))) {
+    stop(x_name, " must contain only point or polygon geometries")
   }
-  if(is.na(sf::st_crs(x))) {
-    stop(deparse(substitute(x)), " must have a defined projection")
+  if (is.na(sf::st_crs(x))) {
+    stop(x_name, " must have a defined projection")
   }
   if (is.null(weight)) {
     wts <- rep(1, nrow(x))
   } else {
     if (!(weight %in% colnames(x))) {
-      stop(weight, "` doesn't exist within ", deparse(substitute(x)))
+      stop(weight, "` doesn't exist within ", x_name)
     }
     wts <- x[[weight]]
     if (any(is.na(wts))) {
@@ -57,7 +58,7 @@ medianCenter <- function (x, group = NULL, weight = NULL) {
     grps <- rep("a", nrow(x))
   } else {
     if (!(group %in% colnames(x))) {
-      stop("Column `",  group, "` doesn't exist within ", deparse(substitute(x)))
+      stop("Column `",  group, "` doesn't exist within ", x_name)
     }
     grps <- x[[group]]
     if (any(is.na(grps))) {
@@ -76,15 +77,15 @@ medianCenter <- function (x, group = NULL, weight = NULL) {
   if (sf::st_is_longlat(x)) {
     stop("Median centroid for unprojected data has not been implemented")
   } else {
-    centerArgs <- data.frame(
-      x = sf::st_coordinates(x)[,1], 
-      y = sf::st_coordinates(x)[,2],
+    ctr_args <- data.frame(
+      x = sf::st_coordinates(x)[, 1],
+      y = sf::st_coordinates(x)[, 2],
       wts = wts)
-    centerArgs_grped <- split(centerArgs, f = grps)
+    ctr_args_split <- split(ctr_args, f = grps)
 
     for (grp in unique_groups) {
-      meansLonLat <- do.call(planarMedianCenter, centerArgs_grped[[grp]])
-      geometry[[grp]] <- sf::st_point(unlist(meansLonLat))
+      mean_xy <- do.call(planar_median, ctr_args_split[[grp]])
+      geometry[[grp]] <- sf::st_point(unlist(mean_xy))
     }
   }
 
@@ -92,8 +93,8 @@ medianCenter <- function (x, group = NULL, weight = NULL) {
     geometry = sf::st_sfc(geometry, crs = sf::st_crs(x))
   ))
   if (!is.null(group)) {
-    output[[group]] = unique_groups
-    output <- output[, c(2,1)]
+    output[[group]] <- unique_groups
+    output <- output[, c(2, 1)]
   }
   output
 }
